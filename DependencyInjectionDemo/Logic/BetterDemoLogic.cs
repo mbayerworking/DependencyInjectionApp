f0e5393748cfcf009ba13190fc637879f0c44d4b
mbayerworking <mbayer.work@gmail.com>
2022-09-08 13:47:11 -0700

    Initial commit

---
+++ DependencyInjectionDemo/Logic/BetterDemoLogic.cs
@@ -0,0 +1,15 @@
+﻿namespace DependencyInjectionDemo.Logic;
+
+public class BetterDemoLogic : IDemoLogic
+{
+    public BetterDemoLogic()
+    {
+        Value1 = 25;
+        Value2 = 50;
+    }
+
+    public int Value1 { get; private set; }
+    public int Value2 { get; private set; }
+
+
+}
---
+++ DependencyInjectionDemo/Logic/DemoLogic.cs
@@ -0,0 +1,14 @@
+﻿namespace DependencyInjectionDemo.Logic;
+
+public class DemoLogic : IDemoLogic
+{
+    public DemoLogic()
+    {
+        Value1 = Random.Shared.Next(1, 1001);
+        Value2 = Random.Shared.Next(1, 1001);
+    }
+
+    public int Value1 { get; private set; }
+    public int Value2 { get; private set; }
+
+}
